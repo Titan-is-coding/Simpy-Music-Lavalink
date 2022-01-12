618c6da11f0f7932db9ec98fa2f1380f93eff055
Noobplayer7 <78253459+Noobplayer77777@users.noreply.github.com>
2022-01-12 20:01:16 +0530

    Update links.js

--- commands/Misc/links.js
+++ commands/Misc/links.js
@@ -6,9 +6,11 @@ module.exports={
   $description[1; 
   **Main Bot Invite Link : **[Invite Me](https://discord.com/api/oauth2/authorize?client_id=915489544026066974&permissions=8&scope=bot%20applications.commands)
 
-**Bot vote link : **[Vote For ME!](https://discordbotlist.com/bots/simpy-music)
+**Alternative Bot invite link : ** [Invite me too](https://discord.com/api/oauth2/authorize?client_id=918426277243924480&permissions=8&scope=bot)
 
-**Alternative Bot invite link : ** [Invite me too](https://discord.com/api/oauth2/authorize?client_id=918426277243924480&permissions=8&scope=bot)]
+**Bot website :** [Website](http://simpy-music.cf/)
+
+**Support server : ** [discord link](https://discord.gg/YSwnwqvQwC)]
   $color[1;#0000FF]
 
   $footer[1;Also Join Support Server to see them in action First!]`
