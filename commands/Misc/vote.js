9a1746c9926e79379bce18e32d2d941b632f33ce
Noobplayer7 <78253459+Noobplayer77777@users.noreply.github.com>
2022-01-12 20:01:48 +0530

    Create vote.js

---
+++ commands/Misc/vote.js
@@ -0,0 +1,16 @@
+module.exports={
+  name:"Vote",
+  aliases:['Vote','votes'],
+  code:`
+  $title[1;**I've Got New Vote Links! VOTE** For me Now!
+]
+  $description[1;
+  **__Heres The Links:__**
+  [Top.gg Vote Link](https://top.gg/bot/915489544026066974)
+  [Discord Bot List](https://discordbotlist.com/bots/simpy-music)
+]
+ $thumbnail[1;https://images-ext-2.discordapp.net/external/nOhQpiXQAckAyPN6haUvbYXSnvLRtV1oaqGQWiZ4Hl4/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/915489544026066974/40b4b7ac041620efd9afdf23a6f31372.webp?width=701&height=701]
+  $color[1;#0000FF]
+
+  $footer[1;In Future there Might be New Vote Rewards]`
+}
